99105095adf7bc55d9613c8696f82fd567de97dc
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2007-09-08 19:33:39 +0000

    ignore dist dir
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@21 79f8d753-db37-0410-980a-393e3d9e3e3e

--- hotwire/builtins/term.py
+++ hotwire/builtins/term.py
@@ -3,7 +3,7 @@ from hotwire.builtin import Builtin, BuiltinRegistry, parseargs
 class TermBuiltin(Builtin):
     """Execute a system command in a new terminal."""
     def __init__(self):
-        super(TermBuiltin, self).__init__('term')
+        super(TermBuiltin, self).__init__('term', nostatus=True)
 
     @parseargs('str')
     def execute(self, context, arg):
