c250e970910bdcc4849dcf8bbe7bd06f659f4b1c
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2007-10-11 13:41:57 +0000

    Remove window split code for term; add commented-out change to make it use menu system.
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@94 79f8d753-db37-0410-980a-393e3d9e3e3e

--- hotwire/sysdep/term_impl/base_term.py
+++ hotwire/sysdep/term_impl/base_term.py
@@ -33,23 +33,22 @@ class TerminalWidget(gtk.VBox):
     <menu action='EditMenu'>
       <menuitem action='Copy'/>
       <menuitem action='Paste'/>
-    </menu>       
+    </menu>
+    <!-- <menu action='ControlMenu'>
+      <menuitem action='SplitWindow'/>
+    </menu> -->
   </menubar>
 </ui>"""         
         self.__actions = [
             ('Copy', None, '_Copy', '<control><shift>c', 'Copy selected text', self.__copy_cb),
             ('Paste', None, '_Paste', '<control><shift>V', 'Paste text', self.__paste_cb),
+            ('SplitWindow', None, '_Split to window', None, 'Turn into toplevel window', self.__split_cb),
         ]
         self.__action_group = gtk.ActionGroup('TerminalActions')
         self.__action_group.add_actions(self.__actions)
         self._stream = stream
         self.__title = title
         self.__header = gtk.HBox()
-        self.__mergelink = hotwidgets.Link()
-        self.__mergelink.set_alignment(0, 0.5)
-        self.__mergelink.set_text('Split as window')
-        self.__mergelink_clickid = self.__mergelink.connect('clicked', self.__on_split_clicked)
-        self.__header.pack_start(self.__mergelink, expand=True)
         self.__msg = gtk.Label('')
         self.__pid = None
         self.__exited = False
@@ -85,14 +84,12 @@ class TerminalWidget(gtk.VBox):
         self.__pid = pid
         self.__msg.set_text('Running (pid %s)' % (pid,))
 
-    def __on_split_clicked(self, l):
+    def __split_cb(self, a):
         self.emit('closed')
-        self.__mergelink.hide()
         w = gtk.Window(gtk.WINDOW_TOPLEVEL)
         w.set_title(self.__title)
         w.add(self)
         w.show_all()
-        self.__mergelink.hide()
 
     def _on_child_exited(self, term):
         _logger.debug("Caught child exited")
