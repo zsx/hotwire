17c018da50bc617edd8f8f49704802f5dd23f2ae
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2007-10-30 23:52:20 +0000

    Don't ellipsize command.
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@215 79f8d753-db37-0410-980a-393e3d9e3e3e

--- hotwire_ui/renderers/ps.py
+++ hotwire_ui/renderers/ps.py
@@ -5,7 +5,7 @@ class ProcessRenderer(TreeObjectsRenderer):
     def _setup_view_columns(self):
         self._insert_propcol('pid', title='PID', ellipsize=False)
         self._insert_proptext('owner_name', title='Owner', ellipsize=False)
-        cmdcol = self._insert_proptext('cmd', title='Command', ellipsize=True)
+        cmdcol = self._insert_proptext('cmd', title='Command', ellipsize=False)
         self._set_search_column(cmdcol)
 
     @menuitem()
