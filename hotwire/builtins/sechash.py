c7824f324a1c8c4f199ca6efab36bac147198e1b
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2007-11-03 23:03:54 +0000

    Make sechash work.
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@240 79f8d753-db37-0410-980a-393e3d9e3e3e

---
+++ hotwire/builtins/sechash.py
@@ -0,0 +1,37 @@
+import os,sys,md5,sha
+
+import hotwire
+from hotwire.builtin import Builtin, BuiltinRegistry
+from hotwire.fs import FilePath
+from hotwire.sysdep.fs import Filesystem
+
+
+class SecHashBuiltin(Builtin):
+    """Create a secure hash from objects or files"""
+    def __init__(self):
+        super(OpenBuiltin, self).__init__('sechash', idempotent=True,
+                                          input='any',
+                                          output=str,
+                                          options=[['-5', '--md5'],])
+
+    def execute(self, context, args, options=[]):
+        alg = ('-5' in options) and md5 or sha  
+        fs = Filesystem.getInstance()
+        if context.input:
+            for val in context.input:
+                valstr = str(val)
+                hashval = alg.new()
+                hashval.update(valstr)
+                yield hashval.hexdigest()
+        for arg in args:
+            fpath = FilePath(arg, context.cwd)
+            stream = open(fpath)
+            hashval = alg.new()
+            buf = stream.read(4096)
+            while buf:
+                hashval.update(buf)
+                buf = stream.read(4096)
+            stream.close()
+            yield hashval.hexdigest()
+
+BuiltinRegistry.getInstance().register(OpenBuiltin())
