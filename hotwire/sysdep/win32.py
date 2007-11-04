1c701f07b38d07ff5b469b8e834e21fdf803d9f4
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2007-11-04 03:07:09 +0000

    Add hack to add Python to default path.
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@248 79f8d753-db37-0410-980a-393e3d9e3e3e

--- hotwire/sysdep/win32.py
+++ hotwire/sysdep/win32.py
@@ -4,3 +4,7 @@ _win_exec_re_str = '(' + ('|'.join(map(lambda x: '(' + re.escape(x) + ')', _path
 win_exec_re = re.compile(_win_exec_re_str, re.IGNORECASE)
 # Better suggestions accepted!  This is used to try to find the entrypoint for a process.
 win_dll_re = re.compile(r'\.((dll)|(DLL)|(drv)|(DRV))$')
+
+# Hack - this is just to integrate better with things like Turbogears on Windows, we
+# actually need a better way of extending the path
+os.environ['PATH'] += r';c:\Python25\Scripts'
