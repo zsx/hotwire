e2bdcf0fea1be2bb1f526c1aa14208ab2dfabd28
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2008-01-31 18:20:54 +0000

    Rebase FileStringMatch on Tree to be less hacky.
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@939 79f8d753-db37-0410-980a-393e3d9e3e3e

--- hotwire_ui/renderers/filestringmatch.py
+++ hotwire_ui/renderers/filestringmatch.py
@@ -23,43 +23,27 @@ import gtk, gobject, pango
 import hotwire
 import hotwire_ui.widgets as hotwidgets
 from hotwire_ui.renderers.file import FilePathRenderer
-from hotwire_ui.render import ClassRendererMapping
+from hotwire_ui.render import ClassRendererMapping, TreeObjectsRenderer
 from hotwire.builtins.fsearch import FileStringMatch
 from hotwire.util import markup_for_match
 
-class FileStringMatchRenderer(FilePathRenderer):
+class FileStringMatchRenderer(TreeObjectsRenderer):
     def __init__(self, *args, **kwargs):
-        if not 'column_types' in kwargs.iterkeys():
-            kwargs['column_types'] = [gobject.TYPE_PYOBJECT, gobject.TYPE_PYOBJECT]
         super(FileStringMatchRenderer, self).__init__(*args,
                                                       **kwargs)
 
     def _setup_view_columns(self):
-        self._setup_icon_path_columns()
-        self._insert_propcol('line_num', title=_('Line'), idx=1)        
+        self._insert_column('path', title=_('Path'))
+        self._insert_column('line_num', title=_('Line'))        
         colidx = self._table.insert_column_with_data_func(-1, 'Match',
                                                           hotwidgets.CellRendererText(),
                                                           self._render_match)
         col = self._table.get_column(colidx-1)
         col.set_spacing(0)
 
-    def _match_for_iter(self, model, iter):
-        return model.get_value(iter, 1)
-
     def _render_match(self, col, cell, model, iter):
-        obj = self._match_for_iter(model, iter)
+        obj = model.get_value(iter, 0)
         matchmarkup = markup_for_match(obj.line, obj.match_start, obj.match_end)
         cell.set_property('markup', matchmarkup)
-
-    def _get_row(self, obj):
-        uprow = super(FileStringMatchRenderer, self)._get_row(obj.path)
-        return uprow + (obj,)
-
-    def get_objects(self):
-        iter = self._model.get_iter_first()
-        while iter:
-            val = self._model.get_value(iter, 1)
-            yield val
-            iter = self._model.iter_next(iter)
 
 ClassRendererMapping.getInstance().register(FileStringMatch, FileStringMatchRenderer)
