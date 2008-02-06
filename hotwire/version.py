d9d3d23c4edf3aede0e20d62923b87d4387cac88
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2008-02-06 02:15:10 +0000

    Bump version to 0.710.
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@985 79f8d753-db37-0410-980a-393e3d9e3e3e

--- hotwire/version.py
+++ hotwire/version.py
@@ -19,7 +19,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
 # THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-__version__ = '0.701'
+__version__ = '0.710'
 
 def svn_version_str():
     if not svn_version_info:
