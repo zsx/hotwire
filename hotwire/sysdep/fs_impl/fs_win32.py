aac27179f70f64beda3c26941da1031985e5fc36
Zeng.Shixin <Zeng.Shixin@79f8d753-db37-0410-980a-393e3d9e3e3e>
2008-02-22 04:05:52 +0000

    Let stat never fail
    Use _stat from msvcrt to implement a fallback when os.stat fails for Win32File.
    
    Don't know the exact reason, but _stat never fails, it's also used in gnuwin32 stat, which stats:
    "Stat displays information about the specified file(s). You do not need any access rights to the file to get this information but you need search rights to all directories named in the path leading to the file"
    
    See:
    http://gnuwin32.sourceforge.net/packages/stat.htm
    
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@1078 79f8d753-db37-0410-980a-393e3d9e3e3e

--- hotwire/sysdep/fs_impl/fs_win32.py
+++ hotwire/sysdep/fs_impl/fs_win32.py
@@ -19,14 +19,29 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
 # THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import os,sys,subprocess,logging
+import os,sys,subprocess,logging, stat
 
 from hotwire.fs import path_normalize
-from hotwire.sysdep.fs import File, BaseFilesystem,iterd_sorted
-from hotwire.sysdep.win32 import win_exec_re
+from hotwire.sysdep.fs import File, BaseFilesystem,iterd_sorted, FileStatError
+from hotwire.sysdep.win32 import win_exec_re, msvcrt
 import win32api, win32con
 
 _logger = logging.getLogger("hotwire.sysdep.Win32Filesystem")
+
+if msvcrt != None:
+    from ctypes import Structure, c_uint, c_ushort, c_short, c_long, byref
+    class Stat32(Structure):
+        _fields_ = [("st_dev", c_uint),
+                    ("st_ino", c_ushort),
+                    ("st_mode", c_ushort),
+                    ("st_nlink", c_short),
+                    ("st_uid", c_short),
+                    ("st_gid", c_short),
+                    ("st_rdev", c_uint),
+                    ("st_size", c_long),
+                    ("st_atime", c_long),
+                    ("st_mtime", c_long),
+                    ("st_ctime", c_long)];
 
 # TODO - implement native "Recycle Bin" trash functionality.
 # involves lots of hackery with shellapi
@@ -83,6 +98,21 @@ class Win32File(File):
 
     def _do_get_hidden(self):
         self._hidden = win32api.GetFileAttributes(self.path) & win32con.FILE_ATTRIBUTE_HIDDEN
+
+    def _do_get_stat(self, rethrow=False):
+        try:
+            super(Win32File, self)._do_get_stat(rethrow)
+        except FileStatError, e:
+            _logger.debug("Failed to stat '%s': %s", self.path, e)
+            if msvcrt != None:
+                _logger.debug("Trying our own wrapper of _stat32")
+                st = Stat32()
+                msvcrt._stat(self.path.encode(sys.getfilesystemencoding()), byref(st))
+                self.stat = (st.st_mode, st.st_ino, st.st_dev, st.st_nlink - 1, st.st_uid,
+                             st.st_gid, st.st_size, st.st_atime, st.st_mtime, st.st_ctime)
+            else:
+                if rethrow:
+                    raise
 
 def getInstance():
     return Win32Filesystem()
--- hotwire/sysdep/win32.py
+++ hotwire/sysdep/win32.py
@@ -29,3 +29,10 @@ win_dll_re = re.compile(r'\.((dll)|(DLL)|(drv)|(DRV))$')
 # Hack - this is just to integrate better with things like Turbogears on Windows, we
 # actually need a better way of extending the path
 os.environ['PATH'] += r';c:\Python25\Scripts'
+
+try:
+    from ctypes import CDLL
+    from ctypes.util import find_library
+    msvcrt = CDLL(find_library("msvcrt"))
+except:
+    msvcrt = None
