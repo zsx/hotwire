4452313b036c1c2073da0191403faf9b2a800a4b
cgwalters <cgwalters@79f8d753-db37-0410-980a-393e3d9e3e3e>
2008-03-13 02:50:26 +0000

    Convert sechash to new API.
    git-svn-id: https://hotwire-shell.googlecode.com/svn/trunk@1138 79f8d753-db37-0410-980a-393e3d9e3e3e

--- hotwire/builtins/sechash.py
+++ hotwire/builtins/sechash.py
@@ -22,37 +22,31 @@
 import os,sys,md5,sha
 
 import hotwire
-from hotwire.builtin import Builtin, BuiltinRegistry, InputStreamSchema, MultiArgSpec
+from hotwire.builtin import builtin_hotwire, InputStreamSchema
 from hotwire.fs import FilePath
 from hotwire.sysdep.fs import Filesystem
 
-class SecHashBuiltin(Builtin):
-    __doc__ = _("""Create a secure hash (default SHA1) from objects or file arguments.""")
-    def __init__(self):
-        super(SecHashBuiltin, self).__init__('sechash', idempotent=True,
-                                             input=InputStreamSchema('any', optional=True),
-                                             output=str,
-                                             argspec=MultiArgSpec('paths'),                      
-                                             options=[['-5', '--md5'],])
-
-    def execute(self, context, args, options=[]):
-        alg = ('-5' in options) and md5 or sha  
-        fs = Filesystem.getInstance()
-        if (not args) and context.input:
-            for val in context.input:
-                valstr = str(val)
-                hashval = alg.new()
-                hashval.update(valstr)
-                yield hashval.hexdigest()
-        for arg in args:
-            fpath = FilePath(arg, context.cwd)
-            stream = open(fpath)
+@builtin_hotwire(idempotent=True,
+                 input=InputStreamSchema('any', optional=True),
+                 output=str,                   
+                 options=[['-5', '--md5'],])
+def sechash(context, *files):
+    _("""Create a secure hash (default SHA1) from objects or file arguments.""")
+    alg = ('-5' in context.options) and md5 or sha  
+    fs = Filesystem.getInstance()
+    if (not files) and context.input:
+        for val in context.input:
+            valstr = str(val)
             hashval = alg.new()
-            buf = stream.read(4096)
-            while buf:
-                hashval.update(buf)
-                buf = stream.read(4096)
-            stream.close()
+            hashval.update(valstr)
             yield hashval.hexdigest()
-
-BuiltinRegistry.getInstance().register_hotwire(SecHashBuiltin())
+    for arg in files:
+        fpath = FilePath(arg, context.cwd)
+        stream = open(fpath)
+        hashval = alg.new()
+        buf = stream.read(4096)
+        while buf:
+            hashval.update(buf)
+            buf = stream.read(4096)
+        stream.close()
+        yield hashval.hexdigest()
